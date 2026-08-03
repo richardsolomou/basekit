98616ed459aa683968c49acd17c38a8fe7f151af
Richard Solomou <richard.s@posthog.com>
2026-08-03 08:11:23 +0300

    fix: use deployed PostHog env vars (#12)

--- src/lib/posthog.ts
+++ src/lib/posthog.ts
@@ -1,12 +1,12 @@
 import posthog from 'posthog-js'
 
-const apiKey = import.meta.env.VITE_PUBLIC_POSTHOG_KEY
-const apiHost = import.meta.env.VITE_PUBLIC_POSTHOG_HOST
+const apiKey = import.meta.env.VITE_POSTHOG_PROJECT_TOKEN
+const apiHost = import.meta.env.VITE_POSTHOG_HOST
 
 if (!apiKey || !apiHost) {
   if (import.meta.env.DEV) {
     throw new Error(
-      'VITE_PUBLIC_POSTHOG_KEY and VITE_PUBLIC_POSTHOG_HOST variables required by PostHog are missing or un-configured, this causes events to be silently missed. This error stops appearing once both variables are configured',
+      'VITE_POSTHOG_PROJECT_TOKEN and VITE_POSTHOG_HOST variables required by PostHog are missing or un-configured, this causes events to be silently missed. This error stops appearing once both variables are configured',
     )
   }
 } else {
